f4457cbc58cf407171efaf97c3a29d31e334bb8b
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-07-21 17:50:50 -0300

    Feat - added new projects configs

--- dailyfood_mobile/babel.config.js
+++ dailyfood_mobile/babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            authentication: "./src/authentication",
+            core: "./src/core",
+            theme: "./src/theme"
+          }
+        }
+      ],
+    ]
+  
   };
 };
+
+
