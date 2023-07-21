e8be573c7d22359a7a4b95aff6ab8e4b1127eb8c
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-07-21 14:15:54 -0300

    Feat - created firsts steps

--- dailyfood_mobile/App.tsx
+++ dailyfood_mobile/App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { colors } from "./src/theme/colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Welcome to dailyfood</Text>
     </View>
   );
 }
@@ -13,8 +12,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.gray[900],
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: colors.gray[400],
   },
 });
