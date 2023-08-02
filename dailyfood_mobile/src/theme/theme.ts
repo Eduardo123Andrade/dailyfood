373df886a5f2b1e578a34b08300c598caadacdc0
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-08-02 18:48:51 -0300

    Feat - Created some other colors

--- dailyfood_mobile/src/theme/theme.ts
+++ dailyfood_mobile/src/theme/theme.ts
@@ -2,26 +2,31 @@ import { colors } from './colors'
 
 export interface Theme {
   backgroundColor: string
-  buttonBackground: string
   buttonLabel: string
   disabled: string
   error: string
   lightIconColor: string
   placeholderColor: string
+  primary: string
+  secondary: string
+  secondaryLight: string
   surfaceColor: string
   textColor: string
 }
 
 const dark: Theme = {
   backgroundColor: colors.gray[900],
-  buttonBackground: colors.blue[900],
   buttonLabel: colors.gray[100],
   disabled: colors.blue[300],
   error: colors.red[600],
   lightIconColor: colors.gray[400],
-  placeholderColor: colors.gray[800],
+  placeholderColor: colors.gray[700],
+  primary: colors.gray[900],
+  secondary: colors.blue[900],
+  secondaryLight: colors.blue[400],
   surfaceColor: colors.gray[800],
   textColor: colors.gray[500],
+  // textLight: colors.gray[]
 }
 
 export const THEMES = {
