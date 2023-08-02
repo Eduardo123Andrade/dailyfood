2bc11fbb91ef42c0c673ff01a231f191e96bb338
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-08-02 19:33:37 -0300

    Fix - fixed options

--- dailyfood_mobile/App.tsx
+++ dailyfood_mobile/App.tsx
@@ -1,22 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { StorageProvider, ThemeProvider } from 'core/providers'
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {
+  NativeStackNavigationOptions,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack'
 import { DailyFoodNavigator } from 'dailyfood'
 
 const Stack = createNativeStackNavigator()
+
+const screenOptions: NativeStackNavigationOptions = {
+  headerShown: false,
+}
 
 export default function App() {
   return (
     <StorageProvider>
       <ThemeProvider>
         <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerShown: false,
-            }}
-          >
-            <Stack.Screen name="DailyFood" component={DailyFoodNavigator} />
+          <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+              name="DailyFood"
+              component={DailyFoodNavigator}
+              options={screenOptions}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </ThemeProvider>
