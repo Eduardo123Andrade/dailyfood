4c6465121be3cb1be7325f4ed2c70138eef685e3
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-08-04 19:37:56 -0300

    Feat - Created delete get post and put hooks

---
+++ dailyfood_mobile/src/core/hooks/useGetRequest.ts
@@ -0,0 +1,42 @@
+import {
+  useQuery,
+  UseQueryResult,
+  UseQueryOptions,
+  MutationStatus,
+  QueryObserverOptions,
+} from 'react-query'
+import { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
+import { useAPI } from './useApi'
+
+export type UseGetRequestStatus = MutationStatus
+
+export type UseGetRequest<TData = any, TError = any> = UseQueryResult<
+  AxiosResponse<TData>,
+  AxiosError<TError>
+>
+
+export type UseGetRequestConfigs = AxiosRequestConfig
+export type UseGetRequestOptions<TData = any, TError = any> = UseQueryOptions<
+  AxiosResponse<TData>,
+  AxiosError<TError>
+>
+
+export const useGetRequest = <TData = any, TError = any>(
+  url: string,
+  options?: QueryObserverOptions<
+    AxiosResponse<TData, TError>,
+    TError,
+    AxiosResponse<TData, TError>
+  >,
+  configs?: UseGetRequestConfigs,
+): UseGetRequest<TData, TError> => {
+  const { API } = useAPI()
+
+  const queryArgs = useQuery<AxiosResponse<TData, TError>, any>({
+    ...options,
+    queryKey: [url, configs],
+    queryFn: () => API.get(url, configs),
+  })
+
+  return queryArgs
+}
---
+++ dailyfood_mobile/src/core/hooks/usePostRequest.ts
@@ -0,0 +1,29 @@
+import {
+  MutationFunction,
+  useMutation,
+  UseMutationOptions,
+  MutationStatus,
+} from 'react-query'
+import { AxiosError, AxiosResponse } from 'axios'
+import { useAPI } from './useApi'
+
+export type UsePostRequestStatus = MutationStatus
+
+export type UsePostRequestOptionsType<TData, TError, TVariables> =
+  UseMutationOptions<AxiosResponse<TData>, AxiosError<TError>, TVariables, any>
+
+export const usePostRequest = <TData = any, TVariables = any, TError = any>(
+  url: string,
+  options?: UsePostRequestOptionsType<TData, TError, TVariables>,
+) => {
+  const { API } = useAPI()
+
+  const mutationFunction: MutationFunction<AxiosResponse<TData>, TVariables> = (
+    data,
+  ) => API.post(url, data)
+
+  return useMutation<AxiosResponse<TData>, AxiosError<TError>, TVariables, any>(
+    mutationFunction,
+    options,
+  )
+}
