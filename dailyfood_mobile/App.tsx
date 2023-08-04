b41cbc93c58621f9f4b44a11edd6cda92940de46
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-08-04 19:38:26 -0300

    Feat - using new providers

--- dailyfood_mobile/App.tsx
+++ dailyfood_mobile/App.tsx
@@ -1,12 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native'
-import { StorageProvider, ThemeProvider } from 'core/providers'
+import {
+  ApiProvider,
+  HttpQueryProvider,
+  StorageProvider,
+  ThemeProvider,
+} from 'core/providers'
 
 import {
   NativeStackNavigationOptions,
   createNativeStackNavigator,
 } from '@react-navigation/native-stack'
-import { DailyFoodNavigator } from 'dailyfood'
 import { AuthenticationNavigator } from 'authentication'
+import { DailyFoodNavigator } from 'dailyfood'
 import { HomeScreen } from 'home/screens/HomeScreen'
 
 const Stack = createNativeStackNavigator()
@@ -18,29 +23,33 @@ const screenOptions: NativeStackNavigationOptions = {
 export default function App() {
   return (
     <StorageProvider>
-      <ThemeProvider>
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={screenOptions}>
-            <Stack.Screen
-              name="DailyFood"
-              component={DailyFoodNavigator}
-              options={screenOptions}
-            />
+      <ApiProvider>
+        <HttpQueryProvider>
+          <ThemeProvider>
+            <NavigationContainer>
+              <Stack.Navigator screenOptions={screenOptions}>
+                <Stack.Screen
+                  name="DailyFood"
+                  component={DailyFoodNavigator}
+                  options={screenOptions}
+                />
 
-            <Stack.Screen
-              name="Authentication"
-              component={AuthenticationNavigator}
-              options={screenOptions}
-            />
+                <Stack.Screen
+                  name="Authentication"
+                  component={AuthenticationNavigator}
+                  options={screenOptions}
+                />
 
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={screenOptions}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </ThemeProvider>
+                <Stack.Screen
+                  name="Home"
+                  component={HomeScreen}
+                  options={screenOptions}
+                />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </ThemeProvider>
+        </HttpQueryProvider>
+      </ApiProvider>
     </StorageProvider>
   )
 }
