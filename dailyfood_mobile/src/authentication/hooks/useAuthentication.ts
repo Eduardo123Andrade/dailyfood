1508586a7eb63d4d37c13d67426a85eedb96f38b
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-08-06 19:01:23 -0300

    Feat - created singup function

--- dailyfood_mobile/src/authentication/hooks/useAuthentication.ts
+++ dailyfood_mobile/src/authentication/hooks/useAuthentication.ts
@@ -1,33 +1,58 @@
-import { Login } from 'authentication/interfaces'
+import { Login, SingUp } from 'authentication/interfaces'
 import { usePostRequest, useUser } from 'core/hooks'
 import { User } from 'core/interfaces'
 
-interface LoginResponse {
+interface AuthResponse {
   token: string
   user: User
 }
 
+const getUser = (data: AuthResponse): User => {
+  const { user: userResponse, token } = data
+  return {
+    token,
+    ...userResponse,
+  }
+}
+
 export const useAuthentication = () => {
   const [, { setUser }] = useUser()
-  const { mutate } = usePostRequest<LoginResponse, Login>('/auth/login', {
-    onSuccess: ({ data }) => {
-      const { user: userResponse, token } = data
-      const user: User = {
-        token,
-        ...userResponse,
-      }
-      setUser(user)
+  const { mutate: loginMutate } = usePostRequest<AuthResponse, Login>(
+    '/auth/login',
+    {
+      onSuccess: ({ data }) => {
+        const user = getUser(data)
+        setUser(user)
+      },
+      onError: (error) => {
+        console.log(JSON.stringify(error, null, 2))
+      },
     },
-    onError: (error) => {
-      console.log(JSON.stringify(error, null, 2))
+  )
+
+  const { mutate: singUpMutate } = usePostRequest<AuthResponse, Login>(
+    '/auth/sing_up',
+    {
+      onSuccess: ({ data }) => {
+        const user = getUser(data)
+        setUser(user)
+      },
+      onError: ({ response }) => {
+        console.log(JSON.stringify(response.data, null, 2))
+      },
     },
-  })
+  )
 
   const login = (data: Login) => {
-    mutate(data)
+    loginMutate(data)
+  }
+
+  const singUp = (data: SingUp) => {
+    singUpMutate(data)
   }
 
   return {
     login,
+    singUp,
   }
 }
