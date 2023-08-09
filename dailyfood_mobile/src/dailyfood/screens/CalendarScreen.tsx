b0f732824a3cfdbad1e20569e25f959c6a002fb1
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-08-09 20:16:34 -0300

    Feat - connected with backend

--- dailyfood_mobile/src/dailyfood/screens/CalendarScreen.tsx
+++ dailyfood_mobile/src/dailyfood/screens/CalendarScreen.tsx
@@ -1,15 +1,31 @@
 import { NativeStackNavigationOptions } from '@react-navigation/native-stack'
 import { Button, Calendar, Screen } from 'core/components'
+import { useMessageModal } from 'core/hooks'
+import { SimpleModal } from 'core/modals'
 import { NavigationHeader } from 'dailyfood/components'
-import React, { useState } from 'react'
+import { useRequestPdf } from 'dailyfood/hooks'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { SPACING } from 'theme'
 
 export const CalendarScreen = () => {
+  const [{ message, show }, { resetState, startMessageModal }] =
+    useMessageModal()
+
   const [selectedDates, setSelectedDates] = useState<string[]>([])
+  const { request, response } = useRequestPdf(startMessageModal)
+
+  useEffect(() => {
+    console.log({ response })
+  }, [response])
 
   const onPress = () => {
-    console.log(JSON.stringify(selectedDates, null, 2))
+    const [firstDate] = selectedDates
+    const lastDate = selectedDates[selectedDates.length - 1]
+
+    if (lastDate < firstDate) return request(lastDate, firstDate)
+
+    request(firstDate, lastDate)
   }
 
   return (
@@ -18,7 +34,15 @@ export const CalendarScreen = () => {
         <Calendar onSelectedDates={setSelectedDates} />
       </View>
 
-      <Button onPress={onPress}>Avançar</Button>
+      <Button disabled={selectedDates.length < 2} onPress={onPress}>
+        Avançar
+      </Button>
+
+      <SimpleModal
+        visible={show}
+        message={message}
+        onRequestClose={resetState}
+      />
     </Screen>
   )
 }
