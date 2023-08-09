b0655fede4285cd65988ec3242c8e7de06967065
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-08-09 20:15:51 -0300

    Created meals mock

--- dailyfood_mobile/src/dailyfood/mock/foods.ts
+++ dailyfood_mobile/src/dailyfood/mock/foods.ts
@@ -5,20 +5,24 @@ export const MOCKED_LIST_FOOD: Food[] = [
     id: '1',
     name: 'Arroz',
     weight: 100,
+    date: new Date(),
   },
   {
     id: '2',
     name: 'Feijão',
     weight: 100,
+    date: new Date(),
   },
   {
     id: '3',
     name: 'Salada',
     weight: 100,
+    date: new Date(),
   },
   {
     id: '4',
     name: 'Arroz',
     weight: 100,
+    date: new Date(),
   },
 ]
---
+++ dailyfood_mobile/src/dailyfood/mock/meels.ts
@@ -0,0 +1,34 @@
+import { Meal } from 'dailyfood/interfaces'
+
+export const MOCKED_LIST_MEAL: Meal[] = [
+  {
+    id: '1',
+    description: 'Café da manha',
+    measurementDate: new Date(),
+    foods: [],
+  },
+  {
+    id: '2',
+    description: 'Lanche',
+    measurementDate: new Date(),
+    foods: [],
+  },
+  {
+    id: '3',
+    description: 'Almoço',
+    measurementDate: new Date(),
+    foods: [],
+  },
+  {
+    id: '4',
+    description: 'Lanche',
+    measurementDate: new Date(),
+    foods: [],
+  },
+  {
+    id: '5',
+    description: 'Jantar',
+    measurementDate: new Date(),
+    foods: [],
+  },
+]
